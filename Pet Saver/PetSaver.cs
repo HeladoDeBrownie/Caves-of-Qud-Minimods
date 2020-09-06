08fdf8008144ed8a3f08da35bf66728ef2004a9e
helado de brownie <helado.de.brownie@gmail.com>
2020-09-05 22:49:57 -0400

    add pet saver

---
+++ Pet Saver/PetSaver.cs
@@ -0,0 +1,23 @@
+namespace XRL.World.Parts
+{
+    [System.Serializable]
+    public class helado_PetSaver_PetSaver : IPart
+    {
+        public override bool WantEvent(int id, int _)
+        {
+            return
+                id == ObjectCreatedEvent.ID ||
+                base.WantEvent(id, _);
+        }
+
+        public override bool HandleEvent(ObjectCreatedEvent _)
+        {
+            if (ParentObject.HasTag("Pet"))
+            {
+                ParentObject.RequirePart<NoDamage>();
+            }
+
+            return true;
+        }
+    }
+}
