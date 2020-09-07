b4acb85bbe38e02e626e58b324c36c452cf39e93
helado de brownie <helado.de.brownie@gmail.com>
2020-09-07 18:50:27 -0400

    add ability to pick a mutation to remove from a list

--- Mutation-B-Gone/Wish.cs
+++ Mutation-B-Gone/Wish.cs
@@ -1,30 +1,70 @@
 using static XRL.UI.Popup;
+using XRL.Wish;
+using XRL.World.Parts;
+using XRL.World.Parts.Mutation;
 
 public class helado_MutationBGone_Wish
 {
-    [XRL.Wish.WishCommand(Command = "mutationbgone")]
+    public const string WishName = "mutationbgone";
+
+    [WishCommand(Command = WishName)]
     public static bool MutationBGone()
     {
-        Show("Usage:\n\nmutationbgone:[mutation id]");
-        return true;
-    }
+        var mutations = GetMutations();
+        var mutationList = mutations.MutationList;
 
-    [XRL.Wish.WishCommand(Command = "mutationbgone")]
-    public static bool MutationBGone(string argument)
-    {
-        var mutations = XRL.Core.XRLCore.Core.Game.Player.Body.GetPart<XRL.World.Parts.Mutations>();
-        var chosenMutation = mutations.GetMutation(argument);
-
-        if (chosenMutation == null)
+        if (mutationList.Count > 0)
         {
-            Show("Didn't find that one. Try again?");
+            var index = ShowOptionList(
+                Title: "Choose a mutation for me to gobble up!",
+
+                Options: mutationList.ConvertAll(delegate (BaseMutation mutation)
+                {
+                    return mutation.DisplayName;
+                }).ToArray(),
+
+                AllowEscape: true
+            );
+
+            if (index != -1)
+            {
+                RemoveMutation(mutations, mutationList[index]);
+            }
         }
         else
         {
-            mutations.RemoveMutation(chosenMutation);
-            Show("Om nom nom! It's gone!");
+            Show("Huh? Get some mutations first if you want me to eat them!");
         }
 
         return true;
+    }
+
+    [WishCommand(Command = WishName)]
+    public static bool MutationBGone(string mutationName)
+    {
+        var mutations = GetMutations();
+        var chosenMutation = mutations.GetMutation(mutationName);
+
+        if (chosenMutation == null)
+        {
+            Show("Didn't find that one. Maybe try wishing just {{Y|mutationbgone}}?");
+        }
+        else
+        {
+            RemoveMutation(mutations, chosenMutation);
+        }
+
+        return true;
+    }
+
+    public static Mutations GetMutations()
+    {
+        return XRL.Core.XRLCore.Core.Game.Player.Body.GetPart<Mutations>();
+    }
+
+    public static void RemoveMutation(Mutations mutations, BaseMutation mutation)
+    {
+        mutations.RemoveMutation(mutation);
+        Show($"Om nom nom! {mutation.DisplayName} is gone! {{{{w|*belch*}}}}");
     }
 }
